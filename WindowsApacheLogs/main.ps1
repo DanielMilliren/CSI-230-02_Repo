b47e9e8de2dc59ebcea84d86522233e70b7de547
Daniel Milliren <daniel.milliren@mymail.champlain.edu>
2024-09-25 14:13:52 -0400

    Parsing Apache Logs Lab done!

--- WindowsApacheLogs/main.ps1
+++ WindowsApacheLogs/main.ps1
@@ -1,3 +1,10 @@
 ﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSSCriptRoot parseApacheLogs.ps1)
 
+#Windows Apache Logs Lab
 searchApacheResponses "/kpop" 404 Mozilla
+
+#Parsing Apache Logs Lab
+$logs = parseApacheLogs
+$logs | Format-Table -AutoSize -Wrap
+$logs.Client
---
+++ WindowsApacheLogs/parseApacheLogs.ps1
@@ -0,0 +1,27 @@
+﻿<#################
+parseApacheLogs()
+Reads and parses Combined Log Format logs
+Output:
+-Pscustomobject listing terms of the log
+#################>
+
+function parseApacheLogs(){
+$logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -lt $logsNotFormatted.Count; $i++){
+$words = $logsNotFormatted[$i].Split(" ");
+    
+    $tableRecords += [pscustomobject]@{ "IP" = $words[0]; `
+                                        "Time" = $words[3].Trim('['); `
+                                        "Method" = $words[5].Trim('"'); `
+                                        "Page" = $words[6]; `
+                                        "Protocol" = $words[7].Trim('"'); `
+                                        "Response" = $words[8]; `
+                                        "Referrer" = $words[10]; `
+                                        "Client" = $words[11..($words.Length - 2)] -join " "; }
+    }
+
+return $tableRecords | Where-Object {$_.IP -ilike "10.*"}
+
+}
